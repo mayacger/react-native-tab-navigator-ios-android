569740ee2ffcd1d158eb809971418901dde21351
mayacger <mayasence@163.com>
2015-12-22 15:55:26 +0800

    创建主文件
    首次提交

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+* mayacger
+* react-native-tab-navigator-ios-android
+**/
+'use strict';
+import { Set } from 'immutable';
+import React, {
+  PropTypes,
+  StyleSheet,
+  View,
+  Component,
+} from 'react-native';
+
+export default class Tab extends Component {
+  constructor (porps) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <View>tab</View>
+    )
+  }
+}
